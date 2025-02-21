5f017c1bfc9aea2b83010b476af1bd8b5364f11c
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-21 17:32:55 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,25 +1,24 @@
-const accessToken =
-  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
-const adAccountId = "1852318231510577"; // ID tài khoản quảng cáo
-const quick_filter = [
+const quickFilter = [
   "Lead Form",
   "Awareness",
   "Engagement",
   "Message",
-  "Pagelike",
   "Traffic",
+  "Pagelike",
 ];
-const goalMapping = {
-  "Lead Form": ["LEAD_GENERATION", "QUALITY_LEAD"],
-  Awareness: ["REACH", "AD_RECALL_LIFT", "IMPRESSIONS"],
-  Engagement: ["POST_ENGAGEMENT", "THRUPLAY", "EVENT_RESPONSES"],
-  Message: ["REPLIES"],
-  Traffic: [
-    "OFFSITE_CONVERSIONS",
-    "LINK_CLICKS",
-    "PROFILE_VISIT",
-    "LADING_PAGE_VIEWS",
-  ],
-  Pagelike: ["PAGE_LIKES"],
-};
-let oodo_view = true;
+const accessToken =
+  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
+
+let isBrand = false;
+const accName = "Viện IDEAS";
+const accAvatar =
+  "https://dom-reports-ideas.netlify.app/DOM-img/logo_ideas.jpg";
+const adAccountId = "1852318231510577";
+// const quickFilter = [
+//   "Haagen Dazs",
+//   "Snowee",
+//   "Esta",
+//   "TRB",
+//   "BEAN",
+//   "Le Petit",
+// ];
