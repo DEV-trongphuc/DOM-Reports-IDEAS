38ffb3b0f7737580bb58b8b7b361622e99f97ca3
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-07-28 11:42:40 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -7,7 +7,7 @@ const quickFilter = [
   "Pagelike",
 ];
 const accessToken =
-  "EAAOahG2GiWkBOZBCDqccffl67c74Tmpjnha7ClZBvxnO9VKcLM4qnH422I9PhE25sOmROOs3hjynmu4tUSBdL6V5BZB1ET3IHAijOtwZAsJpuhZCSe7Vw2ZCA3qJo75EimqmBmThSczRgxN3cBkPafItCsiZA17MXd8ZBDjRGNfjRO8cKspFPtMJUaZCjkkUfO2Uzxrccwoia"; // Token của bạn
+  "EAAOahG2GiWkBPGVhHhxkQTXsgIkiqVEmdRjABTuQVDHTDshQZBvchHuSsZCl0V3gWQpiSMb7qYQg1mdliCw8tdUICJEYrCqOaFrN4MbZCseCXzOGy9r07HDAo1rUs4q0yqerZBwln4iQlaTlubVZA2llHZBjmYRxYlfWBhLUOjlGrFxacZC8ZC5jvZAkBISrM7QArSKHNLt7q"; // Token của bạn
 
 let isBrand = false;
 const accName = "Viện IDEAS";
